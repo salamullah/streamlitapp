3de5e6e654e40301da5027ae4017fae87c0965aa
Salam Ullah Khan <salamullah2003@yahoo.com>
2024-01-16 19:30:45 +0500

    Update streamlitapp.py

--- streamlitapp.py
+++ streamlitapp.py
@@ -1,6 +1,10 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-st.title("Welcome to Streamlit")
-t = np.random.randn(3,3)
-st.write(t)
+
+st.title("Simulation[tm]")
+st.write("Here is our super important simulation")
+
+x = st.slider('Slope', min_value=0.01, max_value=0.10, step=0.01)
+y = st.slider('Noise', min_value=0.01, max_value=0.10, step=0.01)
+
+st.write(f"x={x} y={y}")
